24492ef900190947c65e81474f0ba0fd99ef4d54
Devin Gray <devin.gray@colorado.edu>
2017-11-14 11:06:38 -0700

    reset

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,15 +14,10 @@ export default class Header extends Component {
       <div className='header'>
 
           <div className='nav-bar'>
-            {/* <div className='just-logo'>
-            <img className='dgp' src={require('../pictures/DG.png')}  />
-            </div>
-            <div className='just-links'>
-            <Link className='link' to="/"> Projects </Link>
-            <Link className='link' to="/contact"> Contact </Link>
-            <Link className='link' to="/about"> About </Link>
-            <a href="a-resume"> Resume </a>
-            </div> */}
+
+            <img className='dgp' src={require('../pictures/dev1.png')}  />
+
+
           </div>
 
       </div>
